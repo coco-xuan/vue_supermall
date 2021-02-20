5fb07daebb2f1e6cb2d9396a7abbf5b9d8e0cd43
cocoxuan <1165094065@qq.com>
2021-02-20 15:07:53 +0800

    supermall

--- js/main.js
+++
@@ -1,7 +0,0 @@
-  //-----------------------------调用-------------------------------
-  ; (function (window, undefined) {
-    var map = document.getElementById('map');
-    var game = new Game(map);
-    game.start();
-
-})(window, undefined)
